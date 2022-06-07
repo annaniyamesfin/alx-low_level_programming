267eb89bd130516a3de206c9c0a2996ccd134f85
annaniyamesfin <annanim4@gmail.com>
2022-06-06 17:44:42 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#tdio.h>
 
 /**
 
@@ -14,7 +14,7 @@ int main(void)
   
 {
   
-  printf("with proper grammar, but the outcome is a piece of art,\n");
+  puts("\"Programming is like building a multilingual puzzle");
   
   return (0);
   
